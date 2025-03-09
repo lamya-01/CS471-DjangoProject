0218e5c78375a8ae40e579bdfb135d5bd18e7c50
lamya-01 <lamx7062@gmail.com>
2025-03-10 02:48:16 +0300

    HTML and CSS (Part 2)link,formatting,listing,tables

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -12,4 +12,8 @@ path('', views.index, name= "books.index"),
 path('list_books/', views.list_books, name= "books.list_books"), 
 path('<int:bookId>/', views.viewbook, name="books.view_one_book"), 
 path('aboutus/', views.aboutus, name="books.aboutus"), 
+path("html5/links/", views.html5_links, name="books.html5_links"),
+path("html5/text/formatting/", views.html5_text_formatting, name="books.html5_text_formatting"),
+path("html5/listing/", views.html5_listing, name="books.html5_listing"),
+path("html5/tables/", views.html5_tables, name="books.html5_tables"),
 ] 
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -9,3 +9,16 @@ def viewbook(request, bookId):
     return render(request, 'bookmodule/one_book.html') 
 def aboutus(request): 
     return render(request, 'bookmodule/aboutus.html') 
+
+
+def html5_links(request):
+    return render(request, "bookmodule/html5_links.html")
+
+def html5_text_formatting(request):
+    return render(request, "bookmodule/html5_text_formatting.html")
+
+def html5_listing(request):
+    return render(request, "bookmodule/html5_listing.html")
+
+def html5_tables(request):
+    return render(request, "bookmodule/html5_tables.html")
